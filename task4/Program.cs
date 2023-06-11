e249e9379ced4a2db3573b95843fb87db7dccbd8
maria <antonova.maria.a@gmail.com>
2023-06-11 22:08:28 +0300

    task 4 done

---
+++ task4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int index = 2;
+
+if (num>1){
+while (num >= index) {Console.WriteLine(index);
+index = index+2;
+}}
+else {Console.Write("incorrect input");}
