e746c615854cb302ee6bfed4cd905f9419ce8902
maria <antonova.maria.a@gmail.com>
2023-06-11 21:01:06 +0300

    task 1 done

---
+++ task1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("input first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1>num2) {Console.WriteLine("max = " + num1);}
+else if (num2>num1) {Console.WriteLine("max = " + num2);}
+else {Console.WriteLine("numbers are equal");}
