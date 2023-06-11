b4cdb14da8a24592071df5dc019774b6246c9343
maria <antonova.maria.a@gmail.com>
2023-06-11 22:24:12 +0300

    task 2 done

--- task2/Program.cs
+++ task2/Program.cs
@@ -5,5 +5,14 @@ int num2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("input third number: ");
 int num3 = Convert.ToInt32(Console.ReadLine());
 
+int maxval = num1;
 
+if (num2>maxval) {
+    maxval = num2;
+}
+if (num3>maxval) {
+    maxval = num3;
+}
+Console.WriteLine(maxval); 
+//Так же задачу можно решать через массивы и при увеличении количества вводимых чисел длина кода не меняется, то есть программа будет более оптимизирована
 
