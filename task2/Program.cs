9432eefedf2c7c703b22313e421cc50dac0f7b8e
maria <antonova.maria.a@gmail.com>
2023-06-11 21:32:52 +0300

    task 3 done

---
+++ task2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("input first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input third number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+
+
---
+++ task3/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num%2 == 0) {Console.WriteLine("yes");}
+else {Console.WriteLine("no");}
